6e8ee3340df9b02a4e673a2c65fb3974d157a7ae
AlexisAlejo <116985513+AlexisAlejo@users.noreply.github.com>
2025-11-09 00:59:57 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout';
+
 import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
